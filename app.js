8b0be819f2285aec72654cabe8b2b3de3124dbc5
sfxoozore <oozore@seamfix.com>
2022-11-08 17:02:10 +0100

    Preparing code for deployment

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const cartRoutes = require("./routes/cart");
 const couponRoutes = require("./routes/coupon");
@@ -8,11 +13,15 @@ const errorHandler = require("./middlewares/error-handler");
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
+
+app.use(helmet());
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.json());
 
-app.use(indexRoute);
-app.use(cartRoutes);
-app.use(couponRoutes);
+app.use("/api", indexRoute);
+app.use("/api", cartRoutes);
+app.use("/api", couponRoutes);
 
 app.use(errorHandler);
 
